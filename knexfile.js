945473d72267de884fd36c1da26cd4bfa9fdd075
Cory Ortega <coryortega@Corys-MacBook-Pro.local>
2021-02-27 17:23:26 -0800

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -39,7 +39,8 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    // ssl: { rejectUnauthorized: false },
+    ssl: true,
     migrations: {
       directory: "./database/migrations"
     },
