65d643facb9a73c46d8756efa511cef08e09e4bf
Cory Ortega <coryortega@Corys-MacBook-Pro.local>
2021-02-27 16:28:54 -0800

    attempting to run migrations again

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
-module.exports = {
+var pg = require('pg');
+pg.defaults.ssl = true;
 
+module.exports = {
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
@@ -37,7 +39,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     ssl: true,
     migrations: {
       directory: "./database/migrations"
